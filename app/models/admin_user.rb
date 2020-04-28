9dc4bf7decdce2e8383341208107a19d3415dd70
phungnv <phungnv@larion.com>
2020-04-28 11:39:39 +0700

    add devise user for system

---
+++ app/models/admin_user.rb
@@ -0,0 +1,10 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  belongs_to :role
+  has_many :project_members
+  belongs_to :company
+  has_many :forms
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,33 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2020_04_28_012527) do
 
-  create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.bigint "role_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-    t.index ["role_id"], name: "index_active_admin_comments_on_role_id"
-  end
-
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "account"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "account"
+    t.datetime "remember_created_at"
     t.bigint "company_id"
     t.bigint "role_id"
-    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["company_id"], name: "index_admin_users_on_company_id"
