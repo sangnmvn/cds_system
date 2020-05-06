2a806a9c815c2f207fc40c1696ee7172d46df397
hieuam <minhhieu160498@gmail.com>
2020-05-06 17:18:51 +0700

    change table schedules

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_054545) do
+ActiveRecord::Schema.define(version: 2020_05_06_094551) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -177,13 +177,19 @@ ActiveRecord::Schema.define(version: 2020_05_04_054545) do
   create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "admin_user_id", null: false
     t.bigint "project_id", null: false
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "notify_date"
+    t.date "start_date"
+    t.date "end_date_employee"
+    t.string "notify_employee"
     t.boolean "is_delete", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "company_id"
+    t.text "desc"
+    t.string "status"
+    t.date "end_date_reviewer"
+    t.string "notify_reviewer"
     t.index ["admin_user_id"], name: "index_schedules_on_admin_user_id"
+    t.index ["company_id"], name: "index_schedules_on_company_id"
     t.index ["project_id"], name: "index_schedules_on_project_id"
   end
 
