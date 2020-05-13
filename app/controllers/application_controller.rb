5f047a469c1c644f0400fc33a6f916125e1f550e
ngthduy <nguyenduy170898@gmail.com>
2020-05-13 16:40:40 +0700

    fix message modal cancel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
           }
         }
       else
-        @privilege_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        @privilege_array = Privilege.pluck(:id)
       end
     end
   end
