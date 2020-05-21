13205de91a30bd021c4eda8f279eab5805a2431f
ngthduy <nguyenduy170898@gmail.com>
2020-05-21 10:57:51 +0700

    add filter

--- app/assets/javascripts/form_slot.js
+++ app/assets/javascripts/form_slot.js
@@ -2,25 +2,28 @@ $(document).ready(function () {
   $(".left-panel-competency").hide();
   $("#body-row .collapse").collapse("hide");
   StartLeftPanel();
-
   $("[data-toggle=sidebar-colapse]").click(function () {
     SidebarCollapse();
   });
-  $("[data-toggle=collapse]").click(function () {
-    $(".card table thead tr").css("background-color", "#ccc");
-    $(this).closest("tr").css("background-color", "#8da8db");
-  });
+  // $("[data-toggle=collapse]").click(function () {
+  //   $(".card table thead tr").css("background-color", "#ccc");
+  //   $(this).closest("tr").css("background-color", "#bbcbea");
+  // });
 
   function StartLeftPanel() {
     $("#collapse0").addClass("show");
-    $(".table0 tr").css("background-color", "#8da8db");
+    $(".table0 tr").css("background-color", "#bbcbea");
   }
-
+  $(".card table thead tr").click(function () {
+    $(".collapse").removeClass("show");
+    $(".card-header table tr").css("background-color", "#ccc");
+    id = $(this).data("target");
+    $(id).addClass("show");
+    num = id.split("#collapse");
+    $(".table" + Number(num[1]) + " tr").css("background-color", "#bbcbea");
+  });
 
   function SidebarCollapse() {
-    $(".menu-collapsed").toggleClass("d-none");
-    $(".sidebar-submenu").toggleClass("d-none");
-    $(".submenu-icon").toggleClass("d-none");
     $("#sidebar-container").toggleClass("sidebar-expanded sidebar-collapsed");
 
     if ($(".card").is(":visible")) {
@@ -36,5 +39,12 @@ $(document).ready(function () {
       "fa-angle-double-left fa-angle-double-right"
     );
   }
+  // filter
+  $("#filter-form-slots").multiselect({
+    includeSelectAllOption: true,
+    selectAllValue: "select-all-value",
+  });
+  // $('.filter-slots .multiselect-selected-text').hide();
+  // $('.filter-slots ul li').addClass('active');
 });
 // end
