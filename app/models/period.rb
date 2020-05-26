1f3055d097b09077adfb360684fe31882982d370
phucTNH <phuctran25@outlook.com.vn>
2020-05-26 08:26:40 +0700

    Add validation for Period, Schedule

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,5 @@
 class Period < ApplicationRecord
-    has_many :forms
-    has_one  :schedule
+  has_many :forms
+  has_one :schedule
+  validates :status, inclusion: { in: ["New", "Awaiting Review", "Awaiting Approval", "Pending", "Done"] }
 end
