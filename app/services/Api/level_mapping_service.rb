537b7123b189cf7846ab53d02bf192b81f47be34
phuctran25 <phuctran25@outlook.com.vn>
2020-06-04 20:48:34 +0700

    Update by Level Mapping also keep in mind of Title Mapping

--- app/services/Api/level_mapping_service.rb
+++ app/services/Api/level_mapping_service.rb
@@ -6,20 +6,21 @@ module Api
     end
 
     def get_data_level_mapping
-      level_mappings = LevelMapping.joins([title: [:role]], :user).group([:title_id, :level, :updated_by]).select(:title_id, :level, "MAX(level_mappings.updated_at) as updated_at", :updated_by, "max(rank_number) as no_rank")
-
-      level_mappings.map.each_with_index do |level_mapping, index|
-        #max_updated_title = TitleMapping.select("MAX(updated_at) as updated_at").where(title_id: level_mapping.title_id)&.first&.updated_at
-        #max_updated_title2 = level_mapping.updated_at
-        final_update_date = [max_updated_title, max_updated_title2].max
+      level_mappings = Role.joins([titles: [level_mappings: [:user]]]) \
+      .left_joins(titles: [:title_mapping]) \
+      .select(:id, :level, :name, :first_name, :last_name, \
+      "GREATEST(MAX(level_mappings.updated_at), COALESCE(MAX(title_mappings.updated_at), MAX(level_mappings.updated_at))) as updated_at_max", \
+      "level_mappings.updated_by as updated_by", "max(rank_number) as no_rank") \
+      .group(:id, :level, :name, :first_name, :last_name, :updated_by) 
+      level_mappings.map.each_with_index do |level_mapping, index|        
         {
           no: index + 1,
-          role: level_mapping.title&.role&.name,
+          role: level_mapping.name,
           no_rank: level_mapping.no_rank,
           level: level_mapping.level,
-          latest_update: final_update_date.strftime("%b %d %Y"),
-          updated_by: level_mapping.user&.first_name + " " + level_mapping.user&.last_name,
-          role_id: level_mapping.title&.role&.id,
+          latest_update: level_mapping.updated_at_max&.strftime("%b %d %Y"),
+          updated_by: level_mapping.first_name + " " + level_mapping.last_name,
+          role_id: level_mapping.id,
         }
       end
     end
