fb8772206018d24e453b20e165d604502e83ec25
phucTNH <phuctran25@outlook.com.vn>
2020-07-03 11:24:28 +0700

    Refactor code

--- app/services/Api/export_service.rb
+++ app/services/Api/export_service.rb
@@ -22,22 +22,22 @@ module Api
       company_ids.each do |company_id|
         package = Axlsx::Package.new
         workbook = package.workbook
+        # preparing data
         filtered_data_arr = data_arr.select { |result| result[:company_id] == company_id }
-
         period_prev = filtered_data_arr[0][:period_name_prev]
         period = filtered_data_arr[0][:period_name]
         period_excel_name = filtered_data_arr[0][:period_excel_name]
-
+        # making file
         company_name = filtered_data_arr[0][:company_name].gsub(/ /, "")
         out_file_name = "CDS_Company_#{company_name}_Promotion_Employee_List_in_Period_#{period_excel_name}"
-
+        # formatting Excel
         title_format = workbook.styles.add_style(:sz => 18, :b => true, :bg_color => "FFFFFF", :fg_color => "2E75B8", :font_name => "Calibri", :border => { :style => :thin, :color => "FFFFFF", :edges => [:top, :bottom, :left, :right] }, :alignment => { :horizontal => :left, :vertical => :top, :wrap_text => :true })
         table_header_format = workbook.styles.add_style(:sz => 11, :b => true, :bg_color => "2E75B8", :fg_color => "FFFFFF", :font_name => "Calibri", :border => { :style => :thin, :color => "FFFFFF", :edges => [:top, :bottom, :left, :right] }, :alignment => { :horizontal => :center, :vertical => :top, :wrap_text => :true })
         normal_format = workbook.styles.add_style(:sz => 11, :bg_color => "FFFFFF", :fg_color => "000000", :font_name => "Calibri", :border => { :style => :thin, :color => "000000", :edges => [:top, :bottom, :left, :right] }, :alignment => { :horizontal => :left, :vertical => :top, :wrap_text => :true })
         index_format = workbook.styles.add_style(:sz => 11, :bg_color => "FFFFFF", :fg_color => "000000", :font_name => "Calibri", :border => { :style => :thin, :color => "000000", :edges => [:top, :bottom, :left, :right] }, :alignment => { :horizontal => :center, :vertical => :top, :wrap_text => :true })
         number_format = workbook.styles.add_style(:sz => 11, :bg_color => "FFFFFF", :fg_color => "000000", :font_name => "Calibri", :border => { :style => :thin, :color => "000000", :edges => [:top, :bottom, :left, :right] }, :alignment => { :horizontal => :right, :vertical => :top, :wrap_text => :true })
         email_format = workbook.styles.add_style(:sz => 11, :bg_color => "C0C0C0", :fg_color => "017EAF", :font_name => "Calibri", :border => { :style => :thin, :color => "000000", :edges => [:top, :bottom, :left, :right] }, :alignment => { :horizontal => :left, :vertical => :top, :wrap_text => :true })
-
+        # create sheet
         level_up_sheet = workbook.add_worksheet(:name => "Promotion List")
         level_up_sheet.page_setup.set(fit_to_width: 1)
         level_up_sheet.add_row ["", "", "", "", "", "", "", "", "", ""], :style => title_format
@@ -49,18 +49,13 @@ module Api
         level_up_sheet.add_row ["", "", "", "Title", "Rank", "Level", "Title", "Rank", "Level", "Title"], :style => table_header_format
         level_up_sheet.merge_cells "A3:A4"
         level_up_sheet.merge_cells "B3:B4"
-        #level_up_sheet.merge_cells "C3:C4"
         level_up_sheet.merge_cells "C3:C4"
         level_up_sheet.merge_cells "D3:F3"
         level_up_sheet.merge_cells "G3:I3"
         level_up_sheet.merge_cells "J3:J4"
 
         filtered_data_arr.each_with_index do |result, index|
-          #email_after_formatted = Axlsx::RichText.new
-          #email_after_formatted.add_run(result[:email], :sz => 11, :bg_color => "C0C0C0", :fg_color => "017EAF", :font_name => "Calibri")
-
           level_up_sheet.add_row [index + 1, result[:full_name], result[:email], result[:title_prev], result[:rank_prev], result[:level_prev], result[:title], result[:rank], result[:level], ""]
-
           level_up_sheet.rows[-1].cells.reject.with_index { |element, index| [0, 2, 4, 5, 7, 8].include?(index) }.each { |element| element.style = normal_format }
           level_up_sheet.rows[-1].cells[0].style = index_format
           level_up_sheet.rows[-1].cells[2].style = email_format
@@ -70,8 +65,7 @@ module Api
           level_up_sheet.rows[-1].cells[8].style = number_format
         end
         level_up_sheet.column_widths 5, 30, 30, 20, 5, 5, 20, 5, 5, 30 # run at last
-
-        #
+        # getting output file to public/
         if extension.downcase == "xlsx"
           package.serialize("public/#{out_file_name}.xlsx")
           File.basename("#{out_file_name}.xlsx")
